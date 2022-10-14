80058afdfc1266e5ba79f41a3028a4601c004de2
babar <bapt09.rousseau@gmail.com>
2022-10-14 16:55:31 +0200

    import modification and pep8 modification

--- eda.py
+++ eda.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import collections
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import sklearn as sk
 from sklearn.linear_model import LinearRegression
-import collections
+
 train_data = pd.read_csv("./train.csv")
 length_prots = [len(sequence) for sequence in train_data["protein_sequence"]]
 length_prots = np.array(length_prots)
@@ -22,15 +24,16 @@ train_data.loc[min_ind, ]
 reg = LinearRegression().fit(length_prots_reshaped, train_data["tm"])
 reg.score(length_prots_reshaped, train_data["tm"])
 inds_to = np.where(length_prots > 30000)
-plt.scatter(np.delete(length_prots, inds_to), np.delete(np.array(train_data["pH"]), inds_to), alpha=0.01)
+plt.scatter(np.delete(length_prots, inds_to),
+            np.delete(np.array(train_data["pH"]), inds_to), alpha=0.01)
 plt.show()
 
 # phs
 ph_idiot = np.where(train_data["pH"] > 14)
 lengts_prots_ph_idiot = length_prots[ph_idiot[0]]
-set(train_data.loc[ph_idiot[0],"data_source"])
+set(train_data.loc[ph_idiot[0], "data_source"])
 
-#tm _distrib
+# tm _distrib
 plt.hist(train_data["tm"])
 plt.show()
 
@@ -38,5 +41,6 @@ plt.show()
 dois = collections.Counter(train_data["data_source"])
 
 # mh_tm
-plt.scatter(np.delete(np.array(train_data["pH"]), ph_idiot[0]), np.delete(np.array(train_data["tm"]), ph_idiot[0]), alpha=0.01)
+plt.scatter(np.delete(np.array(train_data["pH"]), ph_idiot[0]),
+            np.delete(np.array(train_data["tm"]), ph_idiot[0]), alpha=0.01)
 plt.show()
