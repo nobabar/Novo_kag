669c8bd7028bae68f2e51ece578a5fa5e51148fb
babar <bapt09.rousseau@gmail.com>
2022-10-21 16:22:31 +0200

    fetch and mutate sequences

--- additional_data.py
+++ additional_data.py
@@ -5,38 +5,42 @@ from operator import itemgetter
 
 import pandas as pd
 import requests
-from Bio import Entrez, SeqIO
+import sqlite3
+from Bio import Seq, SeqIO
 
 with open('databases/thermomutdb.json') as json_file:
     thermomut_db = json.load(json_file)
 
 # filter database
 
-column_keep = ["dtm", "uniprot", "PDB_wild", "mut_count", "effect"]
+# important columns
+column_keep = ["dtm", "uniprot", "mutation_code", "effect"]
 thermomut_db = [x for x in thermomut_db if all(
     v is not None for v in itemgetter(*column_keep)(x))]
 
+# filter invalid uniport ids
+thermomut_db = [x for x in thermomut_db if x["uniprot"].startswith("P")]
 
-# entries with missing values in the following fields are removed
-thermomut_db = [x for x in thermomut_db if all(
-    itemgetter("ph", "length")(x))]
-# by size
-thermomut_db = [x for x in thermomut_db if 100 < x["length"] < 1000]
-# by pH
-thermomut_db = [x for x in thermomut_db if 5 < x["ph"] < 9]
+# filter by pH
+thermomut_db_ph = [x for x in thermomut_db if x["ph"]]
+thermomut_db_ph = [x for x in thermomut_db_ph if 5 < x["ph"] < 9]
 
 
+# convert to dataframe
 thermomut_df = pd.DataFrame(
     [itemgetter(*column_keep)(x) for x in thermomut_db], columns=column_keep)
 
-thermomut_df.groupby("uniprot").ngroups
-thermomut_df.groupby("PDB_wild").ngroups
 
+# make groups
+# get number of groups
+thermomut_df.groupby("uniprot").ngroups
+# make them and filter thoses with 4 or more proteins
 thermomut_df = thermomut_df.groupby(
     "uniprot").filter(lambda x: len(x) >= 4)
-
+# make a new column with the group id
 thermomut_df['gid'] = (thermomut_df.groupby(
     ['uniprot']).cumcount() == 0).astype(int).cumsum()
+
 
 # fetch sequences from uniprot
 thermomut_df["protein_sequence"] = ""
@@ -45,17 +49,21 @@ base_url = "https://www.uniprot.org/uniprot/"
 for index, row in thermomut_df.iterrows():
     currentUrl = base_url + thermomut_df.loc[index, "uniprot"] + ".fasta"
     response = requests.post(currentUrl)
-    Seq = list(SeqIO.parse(StringIO(''.join(response.text)), 'fasta'))[0]
-
+    sequence = str(SeqIO.read(StringIO(''.join(response.text)), 'fasta').seq)
     mutation_code = thermomut_df.loc[index, "mutation_code"]
     match = re.findall(r"([a-z]+)([0-9]+)([a-z]+)", mutation_code, re.I)
-
     for m in match:
-        if Seq.seq[int(m[1])] == m[0]:
-            Seq.seq = Seq.seq[:int(m[1])-1] + \
-                m[2] + Seq.seq[int(m[1]):]
+        if int(m[1]) > len(sequence):
+            m[1] = len(sequence)
+            if sequence[int(m[1])-1] == m[0]:
+                sequence = sequence[:(int(m[1])-1)] + \
+                    m[2] + sequence[int(m[1]):]
+                continue
+        sequence = "invalid"
+        break
+    thermomut_df.loc[index, "protein_sequence"] = sequence
 
-    thermomut_df.loc[index, "protein_sequence"] = str(Seq.seq)
+thermomut_df = thermomut_df[thermomut_df["protein_sequence"] != "invalid"]
 
 # final dataframe
 thermomut_df = thermomut_df[[
