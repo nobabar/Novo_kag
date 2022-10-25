431eedc267d36631406924121e089f47ef615454
Paul Etheimer <paul.eth94@gmail.com>
2022-10-25 09:50:29 +0200

    Added thermo_mut

--- embeddings.py
+++ embeddings.py
@@ -4,16 +4,20 @@ import pandas as pd
 
 
 # Save data as fasta ---------------------------
-def save_data_as_fasta(data, output_file):
+def save_data_as_fasta(data, output_file, col_id):
     with open(output_file, "w") as fileout:
         for _, prot in data.iterrows():
-            fileout.write(f'>protein_id: {prot["seq_id"]}\n')
+            fileout.write(f'>protein_id: {prot[col_id]}\n')
             fileout.write(f'{prot["protein_sequence"]}\n')
 
 
 test_data = pd.read_csv("./test.csv")
 train_data  = pd.read_csv("./train.csv")
+thermo_data = pd.read_csv("./thermomut_grouped.csv", index_col=0)
+max_seq_id = max(test_data["seq_id"])
+thermo_index = list(range(max_seq_id + 1, len(thermo_data) + max_seq_id + 1))
+thermo_data["seq_id"] = thermo_index
 
-save_data_as_fasta(test_data, "test.fasta")
-save_data_as_fasta(train_data, "train.fasta")
-
+save_data_as_fasta(test_data, "test.fasta", "seq_id")
+save_data_as_fasta(train_data, "train.fasta", "seq_id")
+save_data_as_fasta(thermo_data, "thermo.fasta", "seq_id")
--- script_embed.sh
+++ script_embed.sh
@@ -3,5 +3,8 @@
 # python esm/scripts/extract.py esm2_t33_650M_UR50D test.fasta \
 #   test_protein_esm2 --repr_layers 0 32 33 --include mean per_tok
 
-python esm/scripts/extract.py esm2_t33_650M_UR50D train.fasta \
-  train_protein_esm2  --include mean per_tok
+# python esm/scripts/extract.py esm2_t33_650M_UR50D train.fasta \
+#   train_protein_esm2  --include mean per_tok
+
+python esm/scripts/extract.py esm2_t33_650M_UR50D thermo.fasta \
+  thermo_protein_esm2  --include mean per_tok
