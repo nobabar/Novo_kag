a1d917156c5cd730d84574b87e23b266e67e6c22
Paul Etheimer <paul.eth94@gmail.com>
2022-10-25 22:17:00 +0200

    added fused fasta creation and csv

--- embeddings.py
+++ embeddings.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from numpy import index_exp
 import pandas as pd
 
 
@@ -12,12 +13,33 @@ def save_data_as_fasta(data, output_file, col_id):
 
 
 test_data = pd.read_csv("./test.csv")
-train_data  = pd.read_csv("./train.csv")
+train_data  = pd.read_csv("./train_grouped.csv", index_col=0)
 thermo_data = pd.read_csv("./thermomut_grouped.csv", index_col=0)
+
+# Add thermo id
 max_seq_id = max(test_data["seq_id"])
 thermo_index = list(range(max_seq_id + 1, len(thermo_data) + max_seq_id + 1))
 thermo_data["seq_id"] = thermo_index
 
+
+# Add train id
+max_seq_id = max(thermo_data["seq_id"])
+train_index = list(range(max_seq_id + 1, len(train_data) + max_seq_id + 1))
+train_data["seq_id"] = train_index
+
+#rename pre fusion
+thermo_data.rename(columns = {'dtm':'tm', "gid":"group"}, inplace = True)
+
+# separate groups
+max_group_train = max(train_data["group"])
+thermo_data["group"] = thermo_data["group"] + max_group_train
+
+# filter invalid
+thermo_data = thermo_data[thermo_data["protein_sequence"] != "invalid"]
+thermo_data.dropna(axis=0, inplace=True)
+
+# fuse interesting variable
+train_tot = pd.concat([thermo_data[["protein_sequence", "group", "seq_id"]], train_data[["protein_sequence", "group", "seq_id"]]])
+train_tot.to_csv("train_tot.csv")
 save_data_as_fasta(test_data, "test.fasta", "seq_id")
-save_data_as_fasta(train_data, "train.fasta", "seq_id")
-save_data_as_fasta(thermo_data, "thermo.fasta", "seq_id")
+save_data_as_fasta(train_tot, "train_tot.fasta", "seq_id")
--- script_embed.sh
+++ script_embed.sh
@@ -6,5 +6,7 @@
 # python esm/scripts/extract.py esm2_t33_650M_UR50D train.fasta \
 #   train_protein_esm2  --include mean per_tok
 
-python esm/scripts/extract.py esm2_t33_650M_UR50D thermo.fasta \
-  thermo_protein_esm2  --include mean per_tok
+# python esm/scripts/extract.py esm2_t33_650M_UR50D thermo.fasta \
+#   thermo_protein_esm2  --include mean per_tok
+python esm/scripts/extract.py esm2_t33_650M_UR50D train_tot.fasta \
+  train_tot__protein_esm2  --include mean
