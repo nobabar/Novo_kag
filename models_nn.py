3da96820fe5640543e5468ff33d683ee8b3c214d
Paul Etheimer <paul.eth94@gmail.com>
2022-10-26 23:26:06 +0200

    changes but bugs

--- models_nn.py
+++ models_nn.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 
 
-import itertools
 import numpy as np
 import pandas as pd
 import keras
-from keras import Input, Model
-from keras.models import Sequential
-from keras.layers import Activation, Add, Activation, BatchNormalization, AveragePooling1D
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import Conv1D, Flatten
+from keras import Model
+from keras.layers import Concatenate, Input, Dense
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize
 
 import torch
 
@@ -21,58 +17,91 @@ def load_mean_tensors(prot_id_list, direct):
     for i in range(len(prot_id_list)):
         cur_tensor = torch.load(direct + "/protein_id: " + str(prot_id_list[i]) + ".pt")["mean_representations"][33]
         res.append(cur_tensor.tolist())
-    res_np = np.matrix(res)
+    res_np = np.array(res)
     return res_np
 
-def create_conc_XY(prot_data_frame, tensor_list):
-    conc_X = []
-    conc_Y = []
-    groups = set(prot_data_frame["gid"])
-    for group in groups:
-        conc_prot = prot_data_frame[prot_data_frame["gid"] == group]
-        for index in list(itertools.combinations(conc_prot.index,2)):
-            tensor_conc = np.concatenate((
-                np.squeeze(np.asarray(tensor_list[index[0]])),
-                np.squeeze(np.asarray(tensor_list[index[1]]))
-            ))
-            diff_tm = conc_prot.loc[index[0], "dtm"] - conc_prot.loc[index[1], "dtm"]
-            conc_Y.append(diff_tm)
-            conc_X.append(tensor_conc)
-    return (np.matrix(conc_X), np.array(conc_Y))
 
 
+# Data manipulation ------------------------------------------------------------
+# dataframes
+train_data = pd.read_csv("./train_all.csv")
 test_data = pd.read_csv("./test.csv")
-train_data  = pd.read_csv("./train.csv")
-thermo_data = pd.read_csv("./thermomut_grouped.csv", index_col=0)
-max_seq_id = max(test_data["seq_id"])
-thermo_index = list(range(max_seq_id + 1, len(thermo_data) + max_seq_id + 1))
-thermo_data["seq_id"] = thermo_index
+wildtype_data = pd.read_csv("./wildtypes.csv")
 
-thermo_tensors = load_mean_tensors(thermo_data["seq_id"], "./thermo_protein_esm2")
-conc_thermo_tensors, Y = create_conc_XY(thermo_data, thermo_tensors)
+#embeddings
+train_embedding = load_mean_tensors(train_data["seqid"], "./train_protein_esm2")
+test_embedding  = load_mean_tensors(test_data["seq_id"], "./test_protein_esm2")
+wildtype_embedding = load_mean_tensors(wildtype_data["gid"], "./wildtype_protein_esm2")
 
+# Wildtype multiplication
+# group_counts = train_data["gid"].value_counts().sort_index()
 
-# Model
-
-
-def dense_model():
-    model = Sequential()
-    model.add(Dense(32, input_dim=2560, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(1))
-    return model
-
-model_dense = dense_model()
-model_dense.compile(optimizer="adam", loss="mse", metrics=["accuracy"], weighted_metrics=["accuracy"])
-history_dense = model_dense.fit(conc_thermo_tensors, Y, validation_split = 0.2, epochs=20, batch_size=50)
+wt_mult = np.empty((train_data.shape[0], 1280), float)
+for index, row in train_data.iterrows() :
+    group_index_in_wt = int(row["gid"][1:])
+    wt_mult[index, :] = wildtype_embedding[group_index_in_wt, :]
 
 
-plt.plot(history_dense.history['accuracy'])
-plt.plot(history_dense.history['val_accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.plot()
+Y = np.array(train_data["dtm"])
+
+
+# Model ------------------------------------------------------------------------
+
+# simpler model
+#
+Y_normalized = normalize([Y])
+X_train, X_test, Y_train, Y_test = train_test_split(train_embedding, Y_normalized, test_size=0.2)
+
+inputs_spl = Input(shape=(1280,), name="embedding")
+x_spl = Dense(32, "relu")(inputs_spl)
+x_spl = Dense(16, "relu")(x_spl)
+output_spl = Dense(1, "sigmoid")(x_spl)
+
+model_spl = Model(inputs_spl, output_spl)
+model_spl.compile(optimizer=keras.optimizers.Adam(learning_rate=0.1), loss="mean_squared_error", metrics=["mean_squared_error"])
+history = model_spl.fit(X_train, Y_train, validation_split=0.2, epochs=50, batch_size=100)
+
+def plot_loss(history):
+  plt.plot(history.history['loss'], label='loss')
+  plt.plot(history.history['val_loss'], label='val_loss')
+  plt.xlabel('Epoch')
+  plt.ylabel('Error [MPG]')
+  plt.legend()
+  plt.grid(True)
+
+plot_loss(history)
+plt.show()
+
+predictions = model_spl.predict(X_test)
+mse = np.mean((Y_test - predictions)**2)
+
+
+
+def plot_modelspl(x, y):
+  plt.scatter(x, y)
+  plt.xlabel('Horsepower')
+  plt.ylabel('MPG')
+  plt.legend()
+plot_modelspl(Y_test, predictions)
+plt.show()
+
+# Module muté
+
+inputs_mt = Input(shape=1280)
+x_mt = Dense(128, "relu")(inputs_mt)
+out_mt = Dense(64, "relu")(x_mt)
+
+# Module wt
+inputs_wt = Input(shape=1280)
+x_wt = Dense(128, "relu")(inputs_wt)
+out_wt = Dense(64, "relu")(x_wt)
+
+# modele tot
+x = Concatenate()([x_mt, x_wt])
+x = Dense(32, "relu")(x)
+output = Dense(1, "sigmoid")(x)
+
+model = Model([inputs_mt, inputs_wt], output)
+model.compile(optimizer="sgd", loss="mse", metrics=["accuracy"])
+history = model.fit([train_embedding, wt_mult], Y, validation_split=0.2, epochs=20, batch_size=50)
+
