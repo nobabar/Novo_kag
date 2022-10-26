e20a85f006a3208e727dd6040fa13352e5183e78
babar <bapt09.rousseau@gmail.com>
2022-10-26 14:26:08 +0200

    added unique ids for sequences and groups + extracted wildtypes to new file

--- merge_datasets.py
+++ merge_datasets.py
@@ -6,14 +6,23 @@ train_df = pd.read_csv("train_grouped.csv", index_col=0)
 thermomut_df = pd.read_csv("databases/thermomut_grouped.csv", index_col=0)
 
 # prepare for merge
-thermomut_df.drop(columns=["acc_id"], inplace=True)
 thermomut_df.gid = thermomut_df.gid.add(train_df.gid.max())
 
 # merge datasets
 concat_train_df = pd.concat([train_df, thermomut_df], ignore_index=True)
 
 # group by wildtype and redefine gid
-concat_train_df['gid'] = concat_train_df.groupby("wildtype").ngroup().add(1)
+concat_train_df['gid'] = "G" + \
+    concat_train_df.groupby("wildtype").ngroup().astype(str).str.zfill(3)
 concat_train_df.sort_values(by=["gid"], inplace=True)
 
+# reset index and seqid
+concat_train_df.reset_index(drop=True, inplace=True)
+concat_train_df['seqid'] = "S" + concat_train_df.index.astype(str).str.zfill(5)
+
 concat_train_df.to_csv("train_all.csv")
+
+wildtypes = concat_train_df.groupby(
+    "gid").wildtype.unique().apply(lambda x: x[0])
+
+wildtypes.to_csv("wildtypes.csv")
