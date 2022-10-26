0e090393a0f4f00e3379b5a23b9aaf6b458cde31
Paul Etheimer <paul.eth94@gmail.com>
2022-10-26 16:27:40 +0200

    finished tsne (hopefully)

--- tsne.R
+++ tsne.R
@@ -1,43 +1,32 @@
 library(tidyverse)
 library(magrittr)
+library(ggrepel)
+
 train_data <- read_csv("./train_tot_tsne.csv")
+wildtype_data <- read_csv("./wildtype_tsne.csv")
+train_data_2 <- train_data %>%
+  select(gid, tsne_x, tsne_y) %>%
+  mutate(origin = "mutated")
+wildtype_data_2 <- wildtype_data %>%
+  select(gid, tsne_x, tsne_y) %>%
+  mutate(origin = "wildtype")
+
+total_data <- rbind(train_data_2, wildtype_data_2)
+
 ## shapes <- unique(train_data$group)
 ## shapes_df <- tibble("group" = shapes, "shape" = rep(1:5, len = 156))
 ## train_data <- inner_join(train_data, shapes_df, by = "group")
-
-p <- train_data %>%
-  mutate(gid = as.factor(gid)) %>%
+p <- total_data %>%
   ggplot(aes(x = tsne_x, y = tsne_y)) +
-  geom_point(aes(color = gid, shape = gid)) +
-  scale_shape_manual(values = rep(15:18, len = 156))
+  geom_point(aes(color = gid, shape = gid), show.legend = FALSE) +
+  scale_shape_manual(values = rep(15:19, len = length(unique(total_data$gid)))) +
+  geom_label_repel(
+    data = total_data %>% filter(origin == "wildtype"),
+    aes(label = gid, color = NULL),
+    show.legend = FALSE,
+    size = 2
+    ) +
   theme_bw() +
   theme(panel.grid.minor = element_blank())
 
-ggsave("tsne.png", width = 21, height = 9)
-
-
-p2 <- train_data %>%
-  filter(gid %in% c(147, 114)) %>%
-  mutate(gid = as.factor(gid)) %>%
-  ggplot(aes(x = tsne_x, y = tsne_y)) +
-  geom_point(aes(color = gid, shape = gid)) +
-  scale_shape_manual(values = rep(15:18, len = 156))
-  theme_bw() +
-  theme(panel.grid.minor = element_blank())
-
-pb_data <- train_data %>%
-  filter(gid %in% c(0, 85))
-
-pb_data_all <- train_data %>%
-  group_by(tsne_x, tsne_y) %>%
-  filter(n()>1) %>%
-  ungroup() %>%
-  group_by(gid) %>%
-  summarise(n = n())
-
-pb_data_all %>%
-  mutate(gid = as.factor(gid)) %>%
-  ggplot(aes(x = gid, y = n)) +
-  geom_bar(stat = "identity")
-
-ggsave("pb_data.png")
+ggsave("tsne.png", width = 16, height = 9, dpi = 300)
--- unsupervised.py
+++ unsupervised.py
@@ -10,9 +10,13 @@ import matplotlib.pyplot as plt
 np.random.seed(69)#la trik
 
 train_data  = pd.read_csv("./train_all.csv", index_col=0)
-tensors_train = load_mean_tensors(list(train_data.index), "./train_protein_esm2")
+wildtype_data = pd.read_csv("./wildtypes.csv")
+
+
+tensors_train = load_mean_tensors(train_data["seqid"], "./train_protein_esm2")
+tensors_wildtype = load_mean_tensors(wildtype_data["gid"], "./wildtype_protein_esm2")
 tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=10000)
-tsne_results = tsne.fit_transform(tensors_train)
+tsne_results = tsne.fit_transform(np.concatenate((tensors_train, tensors_wildtype)))
 
 train_data['tsne_x'] = tsne_results[:,0]
 train_data['tsne_y'] = tsne_results[:,1]
